0af5b1ce69c1957cb644f762c757acec76ba8e9e
Saket Juneja <saket.juneja@levensystems.com>
2020-08-22 18:39:24 -0400

    fix build

--- CircleCISample/Controllers/HomeController.cs
+++ CircleCISample/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace CircleCISample.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        q3warfq3wa
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
